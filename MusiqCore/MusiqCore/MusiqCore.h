a257238b1d94c85b78551ed3455547d590004af4
Olivier Rigault <rigaulto13@gmail.com>
2020-12-21 23:49:39 +0000

    Add Core framework

---
+++ MusiqCore/MusiqCore/MusiqCore.h
@@ -0,0 +1,18 @@
+//
+//  MusiqCore.h
+//  MusiqCore
+//
+//  Created by Olivier Rigault on 21/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MusiqCore.
+FOUNDATION_EXPORT double MusiqCoreVersionNumber;
+
+//! Project version string for MusiqCore.
+FOUNDATION_EXPORT const unsigned char MusiqCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MusiqCore/PublicHeader.h>
+
+
