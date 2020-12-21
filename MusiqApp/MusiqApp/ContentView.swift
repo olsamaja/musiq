ad0f6fd200f5b9d0cbe7927898e9c77370700a2e
Olivier Rigault <rigaulto13@gmail.com>
2020-12-21 23:45:52 +0000

    Initial commit

---
+++ MusiqApp/MusiqApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusiqApp
+//
+//  Created by Olivier Rigault on 21/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusiqApp/MusiqApp/MusiqAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusiqAppApp.swift
+//  MusiqApp
+//
+//  Created by Olivier Rigault on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MusiqAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
