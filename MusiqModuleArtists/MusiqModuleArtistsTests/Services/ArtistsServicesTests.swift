6ddecc2833d27b573636dd3fc18b724152244f0f
Olivier Rigault <rigaulto13@gmail.com>
2021-04-08 14:43:57 +0100

    Add tests for artists services

---
+++ MusiqModuleArtists/MusiqModuleArtistsTests/Services/ArtistsServicesTests.swift
@@ -0,0 +1,23 @@
+//
+//  ArtistsServicesTests.swift
+//  MusiqModuleArtistsTests
+//
+//  Created by Olivier Rigault on 08/04/2021.
+//
+
+import XCTest
+@testable import MusiqCore
+@testable import MusiqModuleArtists
+@testable import Resolver
+
+class ArtistsServicesTests: XCTestCase {
+
+    func testSuccessfulInjection() throws {
+        
+        Resolver.registerModuleArtistsServices()
+        let viewModel: ArtistsViewModel = Resolver.resolve()
+        
+        XCTAssertNotNil(viewModel)
+    }
+
+}
