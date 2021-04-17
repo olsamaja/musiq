1a94a2cb06a286d7f328d3583dba2e8b635f87cd
Olivier Rigault <rigaulto13@gmail.com>
2021-04-17 23:33:47 +0100

    Add top tags framework

---
+++ MusiqModuleTopTags/MusiqModuleTopTags/Presentation Layer/Views/SearchTopTagsView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchTopTagsView.swift
+//  MusiqModuleTopTags
+//
+//  Created by Olivier Rigault on 17/04/2021.
+//
+
+import SwiftUI
+
+struct SearchTopTagsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchTopTagsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchTopTagsView()
+    }
+}
